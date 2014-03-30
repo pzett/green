e1bcfd645b8606fb25ea99fef0fdde56f9fa4fb6
navaux <navauxmathieu@yahoo.fr>
2014-03-30 21:00:51 +0200

    Power without mean

--- powerEst/powerTot.cpp
+++ powerEst/powerTot.cpp
@@ -10,7 +10,10 @@ float powerTot(Vec<T> datas){
   for (int i=0;i<no_elements;i++){
     power=power+datas[i]*datas[i]/no_elements;
   };*/
-  float power=(float) sum_sqr(abs(datas))/datas.length();
+  T meanDatas=sum(datas)/datas.length();
+  Vec<T> datas2=datas-meanDatas;
+  std::cout << "mean: " << meanDatas <<'\n';
+  float power=(float) sum_sqr(abs(datas2))/datas2.length();
   return power;
 };
 
@@ -20,7 +23,7 @@ int main () {
   myfile.open("datas.txt");
 */
   itpp::vec n="0 , 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15,16,17,18,19";
-  itpp::vec datas=itpp::cos(2*3.14159*0.1*n)+cos(2*3.14159*0.2*n);
+  itpp::vec datas=3+cos(2*3.14159*0.1*n)+cos(2*3.14159*0.2*n);
   std::cout << "datas to be sent:" << datas << '\n';
   float power =powerTot(datas);
   std::cout << "power= " << power << '\n';
