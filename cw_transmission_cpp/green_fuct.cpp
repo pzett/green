ccaf33396a7c51f52a059d78546071480bf1f5d4
navaux <navauxmathieu@yahoo.fr>
2014-04-01 14:06:52 +0200

    documentation

--- cw_transmission_cpp/green_fuct.cpp
+++ cw_transmission_cpp/green_fuct.cpp
@@ -9,18 +9,27 @@
 
 
 using namespace itpp;
-
-
 using namespace std;
 
-
+/** Takes the square of the array
+ * @pre:
+ *       - data_to_harness: a pointer to an array of float
+ *       - no_elements: the length of the array
+ * @post:
+ *       - data_to_harness is now composed with the square of elements
+ */
 void square_elements_of_array(float data_to_harness[],int no_elements) {
   for (int i1=0;i1<no_elements;i1++) {
     data_to_harness[i1]=data_to_harness[i1]*data_to_harness[i1];
   };
 };
 
-
+/** Short description
+ * @pre:
+ * 
+ * @post:
+ *
+ */
 void create_data_CW (short *buffer, int total_num_samps, float nfreq, float Amp )
 {
   short r,i,count;
@@ -34,13 +43,17 @@ void create_data_CW (short *buffer, int total_num_samps, float nfreq, float Amp 
       i=-1*Amp*sin(2*3.1415926*nfreq*count);
       buffer[i1+1] = i;
 
-      count++;
-  
-        
+      count++;      
   };
-
 };
 
+/** Computes the power of the given array
+ * @pre:
+ *       - data: a pointer to an array of short
+ *       - no_elements: the length of the array
+ * @post:
+ *       - power: a short with the power
+ */
 short powerTotArray( short data[], int no_elements){
   short power=0;
   short tmp;
@@ -51,7 +64,14 @@ short powerTotArray( short data[], int no_elements){
 return power;
 };
 
-
+/** Computes the power in the bands centered in 0, 1, ..., 12, -12, -11, ..., -1 
+ * @pre:
+ *       - data_to_harness: a pointer to an array of float
+ *       - no_elements: the length of the array
+ * @post:
+ *       - data_to_harness is now composed with the square of elements
+ */
+/* WRONG!!!
 void powerRange(cvec *data, float *band){
   vec P;
   int data_size=data->size();
@@ -72,6 +92,10 @@ void powerRange(cvec *data, float *band){
 
 
 for (int i=0;i<25;i++){
+  if (i==0){
+  }
+  else {}
   band[i]=sum(P(i*nbr, (i+1)*nbr))*fs/n;
  };
 }
+*/
