1cea8765251dd343285f592e0636427c52d96a80
jslemos <jlemos@kth.se>
2014-04-05 18:23:29 +0200

    offline OFDM test

--- offline/rx_script.m
+++ offline/rx_script.m
@@ -6,35 +6,40 @@
 clear all
 close all
 
-test_n=1;
+test_n=4;
 
-Nsamples_tx=100000;
+Nsamples_tx=100600;
 
 
 %Set USRP values 
 
 Nsamples=2*Nsamples_tx;
 RF_freq=5.5e9; 
-ref_clk=0;
+ref_clock=0;
 gain=15;
-tx_rate=25e6;
-LOoffset=0;
-low_res=0;
+rx_rate=25e6;
+LOoffset=10e6;
 
 %Receive sequence
 
-rx_data=rx(Nsamples, RF_freq, ref_clock, rx_rate, LOoffset);
+rx_data=rx(Nsamples, RF_freq, ref_clock, gain, rx_rate, LOoffset);
 
-figure(1,2,1);
-plot(rx_data);
+rx_data=rx_data/4000;
+
+figure(1)
+subplot(1,2,1);
+plot(rx_data,'o');
+axis([-0.1 0.1 -0.1 0.1])
 title('Data Received from USRP');
 
-figure(1,2,2)
+subplot(1,2,2)
 pwelch(rx_data);
 
 
 %Save data
 
-file_str=['data/tx_data_', num2str(test_n),'.mat'];
+rx_data_4=rx_data;
 
-save('data/rx_data.mat','');
+file_str=['data/rx_data_', num2str(test_n),'.mat'];
+
+%save(file_str,'rx_data_4');
