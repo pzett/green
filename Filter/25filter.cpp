a3e997990a0e120285289cc4c1101170036b5dce
EQ2430 <peterabel23@googlemail.com>
2014-04-08 12:06:02 +0200

    version without segmentation error...

--- Filter/25filter.cpp
+++ Filter/25filter.cpp
@@ -19,8 +19,8 @@ using namespace std;
  * @post:
  *    - y        : output signal
  */
-template <class T,class W>
-void lowPassFilter(T x[],T y[],int nElemIN,W shift)
+//template <class T,class W>
+void lowPassFilter(double x[],double y[],int nElemIN,double shift)
 {
   /* Low pass filter coefficients */
   double a[] ={1.0000,-3.7374,5.2460,-3.2774,0.7689};
@@ -28,34 +28,33 @@ void lowPassFilter(T x[],T y[],int nElemIN,W shift)
   int nElemB = 5;
   int nElemA = 5;
 
-  FILE * xFile;
-  FILE * xShift;
-  FILE * xFilt;
+  // FILE * xFile;
+  // FILE * xShift;
+  // FILE * xFilt;
 
-  double count = 1.00;
-  const double pi = std::acos(-1);
-  const std::complex<double> i(0, 1);
-  T xShifter[nElemIN];
-  T xShifted[nElemIN];
+  //double count = 1.00;
+  //const double pi = std::acos(-1);
+  //const std::complex<double> i(0, 1);
+  double xShifter[nElemIN];
+  double xShifted[nElemIN];
   /* Filling the shifter */
-  for(int f=0;f<nElemIN;f++)
+ int counter = 0;
+ //std::cout << "size: " << nElemIN << std::endl;
+  for(int f=0;f<=nElemIN;f=f+2)
     {
-      xShifter[f] = exp(i*2.00*pi*(-shift)*count);
+      xShifter[f] = cos(-2.00*3.14159*shift*counter);
+      xShifter[f+1] = sin(-2.00*3.14159*shift*counter);
       /* Multiplying the complex arrays elementwise */
-      xShifted[f]=xShifter[f]*x[f];
-      count++;
+      xShifted[f]=xShifter[f]*x[f] - xShifter[f+1]*x[f+1]; // real part
+      xShifted[f+1]=xShifter[f]*x[f+1] + xShifter[f+1]*x[f]; //imag part
+      counter++;
     };
-  xFile = fopen("x.bin","wb");
-  xShift = fopen("xShift.bin","wb");
-  xFilt = fopen("xFilt.bin","wb");
-  fwrite(x, sizeof(std::complex<double>), nElemIN, xFile);
-  fwrite(xShifted,sizeof(std::complex<double>), nElemIN, xShift);
-  fclose(xFile);
-  fclose(xShift);
+  //std::cout << "counter: " << counter << std::endl;
+
   /* filtering */
   filter(b,nElemB,a,nElemA,xShifted,y,nElemIN);
-  fwrite(y,sizeof(std::complex<double>), nElemIN, xFilt);
-  fclose(xFilt);
+  // fwrite(y,sizeof(std::complex<double>), nElemIN, xFilt);
+  // fclose(xFilt);
 }
 
 /** This function lowpass filters an input signalThis function calculates the power of an array
@@ -67,8 +66,8 @@ void lowPassFilter(T x[],T y[],int nElemIN,W shift)
  * @post:
  *    - power        : power of the input signal
  */
-template <class T>
-double powerTotArray(T data[], int no_elements)
+
+double powerTotArray(double data[], int no_elements)
 {
   double power = 0.0;
   double tmp;
@@ -96,17 +95,19 @@ double powerTotArray(T data[], int no_elements)
 
 void powerOfFreqBands(double data[],int nElem, double shiftindex,double power[],int numFilt)
 {
-  complex<double>* dataC = (complex<double>*)data;
-  std::complex<double> tempC[nElem];
+  //complex<double>* dataC = (complex<double>*)data;
+  //std::complex<double> tempC[nElem];
+  double temp[nElem];
   //double power[numFilt];
-  for(int i=0;i<(numFilt);i++)
+  for(int i=0;i<=(numFilt);i++)
     {
       double shift = shiftindex*i;
       //std::cout << "Shift (normalized w0): " << shift  << std::endl;
-      lowPassFilter(dataC,tempC,nElem/2,shift);
-      double* temp = (double*) tempC;
-      power[i] = powerTotArray(temp,nElem/2);
+      lowPassFilter(data,temp,nElem,shift);
+      //double* temp = (double*) tempC;
+      power[i] = powerTotArray(temp,nElem);
       //std::cout << "power: " << power[i] << " at w0: " << shift << std::endl;
+      //std::cout << "numFilt: " << i << std::endl;
     };
 }
 
@@ -120,9 +121,21 @@ int main(){
   double shiftindex = 0.04;//how big the shift between the filters is (equidistantial)
   // creating data
   for(int i=0;i<nElem-1; i=i+2){
-    data[i]=(double)3+3* cos(3.14159*0.05*i);//+cos(2*3.14159*0.2*i);
-    data[i+1]=(double) 10*sin(3.14159*0.4*i);
+    data[i]=(double)3*cos(3.14159*0.3*i);//+cos(2*3.14159*0.2*i);
+    data[i+1]=(double) 10*sin(3.14159*0.1*i);
   };
+
+  double power[numFilt];
+  
+  powerOfFreqBands(data,nElem,shiftindex,power,numFilt);
+ 
+  for(int k=0;k<numFilt;k++)
+    {
+      std::cout << "power: " << power[k] << " " << std::endl;
+    };  
+  return 0;
+}
+
 
   /* Done in the function powerOfFreqBands */
   // complex<double>* dataC = (complex<double>*)data; //casting
@@ -139,13 +152,3 @@ int main(){
   //     //std::cout << "power: " << power[i] << " at w0: " << shift << std::endl;
   //   };
   /* -------------------------------------- */
-
-  double power[numFilt];
-  powerOfFreqBands(data,nElem,shiftindex,power,numFilt);
-
-  for(int k=0;k<numFilt;k++)
-    {
-      std::cout << "power: " << power[k] << " " << std::endl;
-    };  
-  return 0;
-}
