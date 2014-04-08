7734b44ea52f22eaff367556c0b88ff909c5dd50
navaux <navauxmathieu@yahoo.fr>
2014-04-08 10:44:33 +0200

    Transmitter first implementation

---
+++ 1st_assign/TX_MA/tx_funct_MN.hpp
@@ -0,0 +1,12 @@
+//Header of the transmitter 
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+
+void qpsk(double data_bin[], double data_qpsk[], int nElem)
+
+
+void imp_1(short out[])
