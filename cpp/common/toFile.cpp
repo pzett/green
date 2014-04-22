9e6bb1d5d27529a0a8a7a378a1f196afbfbfd5cc
navaux <navauxmathieu@yahoo.fr>
2014-04-22 10:54:51 +0200

    Security on opening file added

--- cpp/common/toFile.cpp
+++ cpp/common/toFile.cpp
@@ -1,4 +1,5 @@
   // Save data to file
      std::ofstream ofs( "sentx_downsamp.dat" , std::ifstream::out );
+if (!ofs.is_open()){std::cerr << "File not open";}
      ofs.write((char * ) buffer, nAll*sizeof(short));
      ofs.close();
